27366d8f1f4a1400060b8a8522f60d95fbf61751
chethtra yen <chethtra_yen@rocketmail.com>
2017-04-01 15:31:17 -0400

    removed cors added middleware

--- server.js
+++ server.js
@@ -4,7 +4,6 @@
 let express = require('express')
 let app = express()
 let bodyParser = require('body-parser')
-let cors = require('cors');
 
 /**
 * App Setup 
@@ -12,7 +11,24 @@ let cors = require('cors');
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
 app.use(express.static('public'))
-app.use(cors())
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
 
 app.use(function (req, res, next) {
 
